e027723a6e0850de6ab149a1fcbb1dba600019f1
Bharati-jaybhay <155735053+bharati-jaybhay@users.noreply.github.com>
2025-02-07 11:12:26 +0530

    added save logic, e-note directory

--- e-note/src/main/java/com/web/e_note/service/NoteService.java
+++
@@ -1,8 +0,0 @@
-package com.web.e_note.service;
-
-import com.web.e_note.entity.Notes;
-import jakarta.validation.Valid;
-
-public interface NoteService {
-    Notes saveNotes(@Valid Notes notes);
-}
